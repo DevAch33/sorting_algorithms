4bedbcb4abf7628672c60db39d7372f5edfd922c
sh15935 <safazsahih@gmail.com>
2023-09-25 15:47:06 -0700

    tasks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,14 +14,14 @@ void bubble_sort(int *array, size_t size)
 {
     int temp;
     size_t i, k;
-    bool swapped;
+    int swapped;
 
     if (!array || !size)
         return;
 
     for (i = 0; i < size - 1; i++)
     {
-        swapped = false;
+        swapped = 0;
         for (k = 0; k < size - i - 1; k++)
         {
             if (array[k] > array[k + 1])
@@ -29,11 +29,11 @@ void bubble_sort(int *array, size_t size)
                 temp = array[k];
                 array[k] = array[k + 1];
                 array[k + 1] = temp;
-                swapped = true;
+                swapped = 1;
                 print_array(array, size);
             }
         }
         if (!swapped)
-            break; // Array is already sorted
+            break; /* Array is already sorted */
     }
 }
